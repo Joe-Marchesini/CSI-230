d22faeb41dd80aa790c116ce1f5a6994906d4da3
joseph.marchesini <joe@pop-os.localdomain>
2020-10-05 19:23:22 -0400

    Project 1 upload

---
+++ Project-Scripting1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# file Project-Scripting1.sh
+# brief  First scripting project with various function implementation
+# author joe.marchesini
+# Project - Scripting 1
+# date 10/5/2020
+
+#Functions
+
+# Outputs system information
+linuxAndSystemInfo()
+{
+}
+
+# Outputs 
