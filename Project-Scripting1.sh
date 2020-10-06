d3e75fa2bfebba00282c483bb2986c70050b598d
Joe-Marchesini <39593578+Joe-Marchesini@users.noreply.github.com>
2020-10-05 20:18:01 -0400

    Update Project-Scripting1.sh

--- Project-Scripting1.sh
+++ Project-Scripting1.sh
@@ -8,9 +8,84 @@
 
 #Functions
 
-# Outputs system information
-linuxAndSystemInfo()
+# brief Outputs system information
+systemInfo()
+{
+  echo "<h2>System Information</h2>"
+  echo "<pre>"
+  uname -a
+  echo "</pre>"
+}
+
+# brief Outputs what memory is free
+freeMemory()
+{
+  echo "<h2>System Information</h2>"
+  echo "<pre>"
+  free -m
+  echo "</pre>"
+}
+
+# brief Outputs avalible disks and disk space
+diskInfo()
+{
+  echo "<h2>Disk Information</h2>"
+  echo "<pre>"
+  df
+  echo "<pre>"
+}
+
+# brief Outputs all useres with a shell prompt
+shellPromptUsers()
+{
+  echo "<h2>System Users</h2>"
+  echo "<pre>"
+  w
+  echo "<pre>"
+}
+
+# brief Outputs system ip
+systemIP()
+{
+  echo "<h2>System IP Address</h2>"
+  echo "<pre>"
+  hostname -I
+  echo "<pre>"
+}
+
+# brief Outputs title, username, and timestamp
+title()
+{
+  echo "<h3>Project Scripting 1 | </h3>"
+  date -d
+  echo "<h3> | </h3>"
+  whoami
+}
+
+# brief Outputs system uptime, based on "Writing Shell Scripts" documentation
+systemUpTime()
+{
+  echo "<h2>System Up Time</h2>"
+  echo "<pre>"
+  uptime
+  echo "<pre>"
+}
+
+# brief Outputs all useres with a shell prompt
+something()
 {
 }
 
-# Outputs 
+#HTML
+
+cat <<- _EOF_
+  <html>
+  <head>
+      <title>$(title)</title>
+  </head>
+
+  <body>
+      $(systemInfo)
+  </body>
+  </html>
+_EOF_
