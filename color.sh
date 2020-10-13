0374a31637780c11ba019112ee3059df6b766372
Joe-Marchesini <39593578+Joe-Marchesini@users.noreply.github.com>
2020-10-12 22:55:04 -0400

    Update color.sh

--- color.sh
+++ color.sh
@@ -6,6 +6,34 @@ blue='/033[0;34m'
 magenta='/033[0;35m'
 default='/033[0m'
 
-read -p "What is your favorit color (red, green, blue, yellow or magenta)?" c
+read -p "What is your favorit color (red, green, blue, yellow or magenta)?" color
 color=${color^^}
-echo "${color} selected"
+
+case ${color} in
+  ${color} = "RED")
+    selected_color=$red
+    ;;
+
+  ${color} = "BLUE")
+    selected_color=$blue
+    ;;
+
+  ${color} = "GREEN")
+    selected_color=$green
+    ;;
+    
+  ${color} = "YELLOW")
+    selected_color=$yellow
+    ;;
+    
+  ${color} = "MAGENTA")
+    selected_color=$magenta
+    ;;
+
+  *)
+    echo "You didn't enter anything!"
+    exit 1
+    ;;
+esac
+
+echo "${selected_color}Your selected color is ${color}${default}"
