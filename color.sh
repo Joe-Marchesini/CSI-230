2c6577024860107118ac18fcdf8ea0247b4dba33
joseph.marchesini <joe@pop-os.localdomain>
2020-10-12 23:31:26 -0400

    color.sh changes

--- color.sh
+++ color.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-red='/033[0;31m'
-green='/033[0;32m'
-yellow='/033[0;33m'
-blue='/033[0;34m'
-magenta='/033[0;35m'
-default='/033[0m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+default='\033[0m'
 
 read -p "What is your favorit color (red, green, blue, yellow or magenta)?" color
 color=${color^^}
 
 case ${color} in
-  "RED")
+  RED)
     selected_color=$red
     ;;
 
-  "BLUE")
+  BLUE)
     selected_color=$blue
     ;;
 
-  "GREEN")
+  GREEN)
     selected_color=$green
     ;;
     
-  "YELLOW")
+  YELLOW)
     selected_color=$yellow
     ;;
     
-  "MAGENTA")
+  MAGENTA)
     selected_color=$magenta
     ;;
 
