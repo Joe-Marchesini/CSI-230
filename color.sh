2a9b6dcde7d63780df114681c92d2d754c158b05
Joe-Marchesini <39593578+Joe-Marchesini@users.noreply.github.com>
2020-10-12 22:22:43 -0400

    Update color.sh

--- color.sh
+++ color.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 red='/033[0;31m'
 green='/033[0;32m'
-yellow=
-blue=
-magenta=
-default=
+yellow='/033[0;33m'
+blue='/033[0;34m'
+magenta='/033[0;35m'
+default='/033[0m'
 
 read -p "What is your favorit color (red, green, blue, yellow or magenta)?" c
 color=${color^^}
